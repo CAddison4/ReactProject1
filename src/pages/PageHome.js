9a893153e663eb3dab4903b730fcd4493da7383a
Catherine Addison <caddison4@my.bcit.ca>
2022-12-08 15:01:02 -0800

    home page changes

--- src/pages/PageHome.js
+++ src/pages/PageHome.js
@@ -39,7 +39,7 @@ const PageHome = () => {
 
     return (
         <section>
-            <h2>Home Page</h2>
+            <h2>Movies Page</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit porro, dolorem, quod facere enim voluptate provident quo labore vero repellat nemo animi ad exercitationem rem quos, possimus libero deleniti laudantium?</p>
         </section>
     );
